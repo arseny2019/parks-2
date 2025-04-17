fcb5e27fa1df0c14e6f35cf6ea8b4d92dad8c49c
znamenskii_aa1 <aznamenskij@primegroup.msk.ru>
2025-04-17 18:16:36 +0400

    upd после переноса url

--- helpers/directus.js
+++ helpers/directus.js
@@ -1,4 +1,4 @@
 export const publicUserToken = 'gwJ1CgG1keVVigouqkp1CVF0Zeqj-z6p';
-const directusAPIUrl = 'https://dir.parksdev.ru';
+const directusAPIUrl = 'https://xn--80aimpg.xn--80aqfsg.xn--p1ai';
 export const getImageURL = (imageId) => `${directusAPIUrl}/assets/${imageId}?access_token=${publicUserToken}&q=100`
 // export const getImageURL = async (imageId) => await directus.request(readAssetRaw(imageId, { quality: 100, access_token: publicUserToken }));
