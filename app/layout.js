4087d7619def08a43c3aa309124887c1caf00c08
znamenskii_aa1 <aznamenskij@primegroup.msk.ru>
2025-05-19 12:59:39 +0400

    fix ;

--- app/layout.js
+++ app/layout.js
@@ -34,7 +34,7 @@ export default function RootLayout({children}) {
             >
             <Suspense>
                 <YandexMetrikaContainer enabled={true} />
-            </Suspense>;
+            </Suspense>
             {children}
             </body>
         </html>
